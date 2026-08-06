bb95c8710a7e28d6adfca2e7b08797678d299b94
onyo <daniluca091@gmail.com>
2026-08-06 11:12:35 +0300

    add github actions for build and release

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+apt update
+apt install -y live-build debootstrap systemd curl
+
+chmod +x auto/*
+
+echo "Configuring the build"
+lb config
+
+echo "Building the image"
+lb build
+
+echo "Build complete"
